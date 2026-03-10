e8697651817325db14ef0ed01cff8c530d544ade
martinkSH <martink@sayhueque.com>
2026-03-10 17:52:40 -0300

    Update TicketModal.tsx

--- components/TicketModal.tsx
+++ components/TicketModal.tsx
@@ -81,7 +81,7 @@ export default function TicketModal({ ticket, responsables, perfil, onClose, onU
         </div>
 
         {/* Body */}
-        <div className="p-6 space-y-5">
+        <div className="p-6 space-y-5 overflow-y-auto flex-1">
 
           {/* Info del solicitante */}
           <div className="grid grid-cols-2 gap-4">
